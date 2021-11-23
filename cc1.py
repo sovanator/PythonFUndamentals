e73b301775a3424c5550d0aa831188bf54c94109
sovanator <sunham.kc@gmail.com>
2021-11-22 20:22:54 -0800

    just playing around

---
+++ cc1.py
@@ -0,0 +1,15 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b= 12.1
+print(type(b))
+
+c="Subham"
+print(type(c))
+
+d = a + b
+print(d)
